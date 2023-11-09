d5307cfc5425e825b60bad870314c5174811004a
Andris Degtjarevs <andrikd1@inbox.lv>
2023-11-09 21:07:51 +0200

    [feature/09112023] : Classroom work

---
+++ src/main/java/lv/acodemy/MethodExercises.java
@@ -0,0 +1,65 @@
+package lv.acodemy;
+
+public class MethodsExercises {
+    public static void main(String[] args) {
+
+        System.out.println(Math.round(fahrenheitConverter(120)));
+        System.out.println(fahrenheitConverter(120));
+
+        System.out.println(currencyConverter("CZK", 50));
+        System.out.println(currencyConverter("GBP", 120));
+
+
+        System.out.println(isPalindrome("privet"));
+    }
+
+    public static double fahrenheitConverter(double fahrenheit) {
+        return (fahrenheit - 32) / 1.80000;
+    }
+    // PLN // CZK // GBP
+
+    public static double currencyConverter(String currency, double money) {
+        double convertedAmount;
+        switch (currency) {
+            case "PLN":
+                convertedAmount = money * 4.45;
+                break;
+            case "CZK":
+                convertedAmount = money * 24.55;
+                break;
+            case "GBP":
+                convertedAmount = money * 0.87;
+                break;
+            default:
+                throw new RuntimeException("Unsupported currency: " + currency);
+        }
+        return convertedAmount;
+    }
+
+    public static boolean isPalindrome(String word) {
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+        return reversed.equals(word);
+    }
+
+    public static double calculator(String operation, double firstNumber, double secondNumber) {
+        double result = 0;
+        switch (operation) {
+            case "+":
+                result = firstNumber + secondNumber;
+                break;
+            case "-":
+                result = firstNumber - secondNumber;
+                break;
+            default:
+                throw new RuntimeException("Invalid operation:" + operation)
+        }
+    }
+
+}
+
+
+
+
---
+++ src/main/java/lv/acodemy/Methods.java
@@ -0,0 +1,34 @@
+package lv.acodemy;
+
+public class Methods {
+    public static void main(String[] args) {
+
+
+        printSomeText(10, "acodemy");
+        System.out.println(calculateSumOfTwoNumbers(20, 40));
+
+        int sumOfTwoNumbers = calculateSumOfTwoNumbers(10, 20);
+        System.out.println(sumOfTwoNumbers);
+
+        printInfo("", "", 21);
+
+    }
+
+    public static void printSomeText(int loopCount, String text) {
+
+        for (int i = 0; i <= loopCount; i++) {
+            System.out.println(text);
+
+        }
+    }
+
+    public static int calculateSumOfTwoNumbers(int firstNumber, int secondNumber) {
+        return firstNumber + secondNumber;
+
+    }
+
+    public static void printInfo(String name, String lastName, int age) {
+        System.out.printf("My name is %s. My surname is %s. I am %d years old.",
+                name, lastName, age);
+    }
+}
